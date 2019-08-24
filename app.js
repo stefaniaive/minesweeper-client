27bd3fc27a824d6eaa340b0b01c5031cd74f60aa
Stefania Ive <stefaniaive@gmail.com>
2019-08-24 01:07:42 -0300

    create a little client for minesweeper API

---
+++ app.js
@@ -0,0 +1,4 @@
+var minesweeper = require('./index.js');
+
+//console.log(minesweeper.startNewGame(3,3,1));
+console.log(minesweeper.clickCell("9b9659e4-0a52-43b3-a531-5ecfa8136c2c",0,0))
---
+++ index.js
@@ -0,0 +1,38 @@
+const request = require('request');
+const base_url = "http://localhost:5000/api/minesweeper"
+
+
+exports.startNewGame = function(sizeX, sizeY, mines) {
+	request.post(base_url, {
+	  json: {
+	  	"sizeX": sizeX,
+		"sizeY": sizeY,
+		"mines": mines	  
+		}
+	}, (error, res, body) => {
+	  if (error) {
+	    console.error(error)
+	    return
+	  }
+	  console.log(`statusCode: ${res.statusCode}`)
+	  console.log(body)
+	})
+
+
+}
+
+exports.clickCell = function(id, posX, posY) {
+	request.patch(base_url + "/" + id, {
+	  json: {
+	    "posX": posX,
+    	"posY": posY
+		}
+	}, (error, res, body) => {
+	  if (error) {
+	    console.error(error)
+	    return
+	  }
+	  console.log(`statusCode: ${res.statusCode}`)
+	  console.log(body)
+	})
+}
